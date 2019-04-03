ba27afdf25af44563b3509383563bba9db30341a
Barbara Fick <bkantzos@yahoo.com>
2019-04-03 13:41:18 -0600

    setting up node/express project with authentication

---
+++ models/User.js
@@ -0,0 +1,48 @@
+
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstname: {
+      type: String,
+      unique: false,
+      required: true
+  },
+  lastname: {
+      type: String,
+      unique: false,
+      required: true,
+  },
+  email: {
+      type: String,
+      unique: true,
+      required: true
+  },
+  username: {
+      type: String,
+      unique: true,
+      required: true
+  },
+  password: {
+      type: String,
+      unique: false,
+      required: true
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now()
+  }
+});
+
+usersSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+}
+
+userSchema.methods.compareSync = function(password, encrypted) {
+    return bcrypt.compareSync(password, encrypted);
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
